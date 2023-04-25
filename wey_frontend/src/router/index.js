45d799d6681f9cd37711dd069af4abfea50a4e6c
Andrey Vorontsov <reygofor@gmail.com>
2023-04-25 13:34:45 +0300

    add log in page

--- wey_frontend/src/router/index.js
+++ wey_frontend/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import LoginView from '../views/LoginView.vue'
 import SignupView from '../views/SignupView.vue'
 
 const router = createRouter({
@@ -14,6 +15,11 @@ const router = createRouter({
 			path: '/signup',
 			name: 'signup',
 			component: SignupView
+		},
+		{
+			path: '/login',
+			name: 'login',
+			component: LoginView
 		},
 		{
 			path: '/about',
--- wey_frontend/src/views/LoginView.vue
+++ wey_frontend/src/views/LoginView.vue
@@ -2,7 +2,7 @@
 	<div class="max-w-7xl mx-auto grid grid-cols-2 gap-4">
 		<div class="main-left">
 			<div class="p-12 bg-white border border-grey-200 rounded-lg">
-				<h1 class="mb-6 text-2xl">Sign up</h1>
+				<h1 class="mb-6 text-2xl">Log in</h1>
 				<p class="mb-6 text-gray-500">
 					Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae
 					repudiandae, nihil nam itaque, iure, aut accusamus error recusandae
@@ -10,25 +10,17 @@
 					earum magni id?
 				</p>
 				<p class="font-bold">
-					Already have an account?
-					<RouterLink :to="{ name: 'login' }" class="underline"
+					Don't have an account?
+					<RouterLink :to="{ name: 'signup' }" class="underline"
 						>Click here</RouterLink
 					>
-					to log in!
+					to create one!
 				</p>
 			</div>
 		</div>
 		<div class="main-right">
 			<div class="p-12 bg-white border border-grey-200 rounded-lg">
 				<form class="space-y-6">
-					<div>
-						<label>Name</label><br />
-						<input
-							type="text"
-							placeholder="Your full name"
-							class="w-full mt-2 py-4 px-6 border border-gray-200 rounded-lg"
-						/>
-					</div>
 					<div>
 						<label>E-mail</label><br />
 						<input
@@ -46,16 +38,8 @@
 						/>
 					</div>
 					<div>
-						<label>Repeat password</label><br />
-						<input
-							type="password"
-							placeholder="Repeat your password"
-							class="w-full mt-2 py-4 px-6 border border-gray-200 rounded-lg"
-						/>
-					</div>
-					<div>
 						<button class="py-4 px-6 bg-purple-600 text-white rounded-lg">
-							Sign up
+							Log in
 						</button>
 					</div>
 				</form>
