21025b3b366358b3cb8611fcbcca8de3c67f6a06
Andrey Vorontsov <reygofor@gmail.com>
2023-04-26 10:33:40 +0300

    add messages page

--- wey_frontend/src/router/index.js
+++ wey_frontend/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import FeedView from '../views/FeedView.vue'
 import HomeView from '../views/HomeView.vue'
 import LoginView from '../views/LoginView.vue'
+import MessagesView from '../views/MessagesView.vue'
 import SignupView from '../views/SignupView.vue'
 
 const router = createRouter({
@@ -26,6 +27,11 @@ const router = createRouter({
 			path: '/feed',
 			name: 'feed',
 			component: FeedView
+		},
+		{
+			path: '/messages',
+			name: 'messages',
+			component: MessagesView
 		},
 		{
 			path: '/about',
